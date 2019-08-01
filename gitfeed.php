d30b77cae2059de7f9254e19fd36d22f7cd20804
apieschel <apieschel@gmail.com>
2019-08-01 15:03:09 -0500

    converted first set of sync requests to async

--- gitfeed.php
+++ gitfeed.php
@@ -31,6 +31,7 @@ function gf_git_feed() {
 	$url = 'https://api.github.com/users/' . $user . '/repos';
 	$headers = array('Authorization' => 'Basic '.base64_encode("$user:$password"), 'User-Agent' => $user);
 	$wpget = wp_remote_get( $url, array('headers' => $headers) );
+	//var_dump($wpget);
 	$data = json_decode($wpget["body"]);
 	$repos = array();
 	
@@ -58,9 +59,11 @@ function gf_git_feed() {
 			$repos = array_slice($repos, 0, 10, TRUE);
 		}
 		
+		$args = array();
 		$commits = array();
 		$commit_stats = array();
 		
+		/*
 		// loop to grab latest commit from each repo
 		foreach($repos as $key=>$value) {
 			$url = 'https://api.github.com/repos/' . $user . '/' . $value[0] . '/commits';
@@ -68,12 +71,35 @@ function gf_git_feed() {
 			$wpget = wp_remote_get( $url, array('headers' => $headers) );
 			$data = json_decode($wpget["body"]);
 			array_push($commits, $data);
+		} */
+		
+		// use built-in Wordpress Requests class to send multiple aynchronous requests
+		foreach($repos as $key=>$value) {
+			$url = 'https://api.github.com/repos/' . $user . '/' . $value[0] . '/commits';
+			$headers = array('Authorization' => 'Basic '.base64_encode("$user:$password"));
+			array_push($args, array('type' => 'GET', 'headers' => $headers, 'url' => $url));
 		}
+		
+		$responses = Requests::request_multiple($args);
 
-		// loop to grab commit stats and patch data for each commit
+		foreach ($responses as $response) {
+				if ($response->status_code !== 200) {
+						echo 'We got a ' . $response->status_code . ' error on our hands.<br><br><br>';
+						break;
+				}
+				// handle success
+				$data = json_decode($response->body);
+				//var_dump($data);
+				$commits[strtotime($data[0]->commit->author->date) - (60 * 60 * 5)] = $data[0];
+		}
+		
+		krsort($commits);
+		$commits = array_values($commits);
+
+		// loop to grab latest commit stats and patch data for each commit
 		foreach($commits as $key=>$value) {
-			$sha = $value[0]->sha;
-			$url = $value[0]->url;
+			$sha = $value->sha;
+			$url = $value->url;
 			$headers = array('Authorization' => 'Basic '.base64_encode("$user:$password"), 'User-Agent' => $user);
 			$wpget = wp_remote_get( $url, array('headers' => $headers) );
 			$data = json_decode($wpget["body"]);
@@ -83,18 +109,18 @@ function gf_git_feed() {
 		// display the data
 		echo '<div class="container-fluid">';
 			echo '<h2 style="font-size:1.4rem; font-weight:normal; text-align:center; margin-bottom:20px;">This custom WordPress plugin displays a feed of ' . $user . '&apos;s Git repos, sorted from the most recently updated.</h2>';
-			echo '<p style="text-align:center; margin-bottom:40px;"><a target="_blank" style="color:#0000EE;" href="https://github.com/' . $user . '">Link to' . $user . '&apos;s Github Page</a></p>';
+			echo '<p style="text-align:center; margin-bottom:40px;"><a target="_blank" style="color:#0000EE;" href="https://github.com/' . $user . '">Link to ' . $user . '&apos;s Github Page</a></p>';
 			
 			$count = 0;
 			foreach($repos as $key=>$value) {	
 				echo '<div style="background:#edffed; border:1px solid lightgrey; margin:0 auto; margin-bottom:20px; padding:40px; width:75%;">';
-					echo '<h3 style="text-align:center; font-size:28px; margin-bottom:20px;"><strong>' . $value[0] . '</strong>: ' . $value[1] . '</h3>';
+					echo '<h3 style="text-align:center; font-size:1.3rem; margin-bottom:20px;"><strong>' . $value[0] . '</strong>: ' . $value[1] . '</h3>';
 					echo '<p><span style="color:green;"><em>';
 					esc_html_e('Last updated', 'gitfeed');
 					echo '</em>: ' . date("F j, Y, g:i a", $key) . ' U.S. Central Time</span></p>';
 				
 					$response = $commits[$count];
-					echo '<p><em>Latest Commit</em>: ' . $response[0]->commit->message . '</p>';
+					echo '<p><em>Latest Commit</em>: ' . $response->commit->message . '</p>';
 				
 					$response2 = $commit_stats[$count];
 					echo '<p><em>Total Code Changes</em>: ' . $response2->stats->total . '</p>';	
