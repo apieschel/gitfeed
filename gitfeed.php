934303adf6787185731748aa8e5bb2c076df2ab7
apieschel <apieschel@gmail.com>
2019-08-02 13:00:32 -0500

    admin page setup

--- gitfeed.php
+++ gitfeed.php
@@ -126,3 +126,24 @@ function gf_git_feed() {
 		echo '</div>';
 	}
 }
+
+// Set up options page.
+// https://www.smashingmagazine.com/2016/03/making-a-wordpress-plugin-that-uses-service-apis/
+add_action( "admin_menu", "gf_plugin_menu_func" );
+function gf_plugin_menu_func() {
+   add_submenu_page( "options-general.php",  // Which menu parent
+                  "Gitfeed",            // Page title
+                  "Gitfeed",            // Menu title
+                  "manage_options",       // Minimum capability (manage_options is an easy way to target administrators)
+                  "gitfeed",            // Menu slug
+                  "gf_plugin_options"     // Callback that prints the markup
+               );
+}
+
+// Print the markup for the page
+function gf_plugin_options() {
+   if ( !current_user_can( "manage_options" ) )  {
+      wp_die( __( "You do not have sufficient permissions to access this page." ) );
+   }
+   echo "Hello world!";
+}
